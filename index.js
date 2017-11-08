14a76b5e0e638dcbc375fcb208f3f74bba89cc70
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-11-08 16:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -102,7 +102,9 @@ if (top < 400) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
-  }
+}
+}
+
 window.requestAnimationFrame(step);
   // We should kick of the animation of the rock around here
 moveRock(rock);
